62a8f86bb326ba84bb0f55628670dc4cc13a7387
Mo-Jo <mo.juseph@gmail.com>
2019-09-09 19:21:04 -0400

    finished the main component's shell

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -9,25 +9,37 @@ class Main extends Component {
     render(){
         return(
             <>
-                <Container>
-                <Row>
-                <Col xs={6} md={4}>
-                <Image src={Zodiac} roundedCircle/>
-                </Col>
-                </Row>
-                </Container>
-            
-            
-            <Container>
-                <h1>Zodiac</h1>
-            </Container>
-            
-            
-            <Button variant="success">LogIn</Button>
-        
-            
-            <Button variant="warning">SignUP</Button>
-            
+                <div className='imgCont'>
+                    <Container className='A'>
+                        <Row className='a'>
+                            <Col xs={8} md={4} lg={3}>
+                                    <Image className='imgLogo mx-auto d-block' alt='aligment' src={Zodiac} roundedCircle fluid/>
+                            </Col>
+                        </Row>
+                    </Container>
+                    <Container className='B'>
+                        <Row className='b'>
+                            <Col xs={6} md={8} lg={8}>
+                                Zodiac
+                            </Col>
+                        </Row>
+                    </Container>
+                    
+                    <Container className='C'>
+                        <Row className='c'>
+                            <Col xs={12} md={4} lg={2}>
+                                    <Button variant="success" size='lg'>LogIn</Button>  
+                            </Col>
+                        </Row>
+                    </Container>
+                    <Container className='D'>
+                        <Row className='d'>
+                            <Col xs={12} md={4} lg={2}>
+                                    <Button variant="warning" size='lg'>SignUP</Button>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div> 
             </>
         )
     }
