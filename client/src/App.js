cf2550fb753328b6623628a6c035fa28a6563084
Mo-Jo <mo.juseph@gmail.com>
2019-09-09 20:48:07 -0400

    intiated the signup Page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter , Route } from 'react-router-dom';
 import Main from './components/Main/Main';
 import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 
 
 
@@ -19,6 +20,7 @@ class App extends Component {
         <div className='App'>
           <Route exact path='/' component={Main}/>
           <Route exact path='/login' component={Login}/>
+          <Route exact path='/signup' component={Signup}/>
         </div>
       </BrowserRouter>
       </>
--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,10 +1,10 @@
 import React , {Component} from 'react';
 import Zodiac from './assets/img/zodiac.png';
-import './assets/style/styleLogin.css';
+import './assets/style/styleSignup.css';
 import { Button, Container, Row, Col, Image, Form } from 'react-bootstrap';
 
 
-class Login extends Component {
+class Signup extends Component {
 
     render(){
         return(
@@ -21,15 +21,28 @@ class Login extends Component {
                         <Row className='b'>
                             <Col xs={6} md={8} lg={8}>
                                 <Form>
+                                <Form.Group controlId="formBasicEmail">
+                                        <Form.Label>Name</Form.Label>
+                                        <Form.Control type="email" placeholder="Enter Name" />
+                                    </Form.Group>
+
                                     <Form.Group controlId="formBasicEmail">
                                         <Form.Label>Username</Form.Label>
-                                        <Form.Control type="email" placeholder="Enter email" />
+                                        <Form.Control type="email" placeholder="Enter Username" />
                                     </Form.Group>
 
                                         <Form.Group controlId="formBasicPassword">
                                             <Form.Label>Password</Form.Label>
                                             <Form.Control type="password" placeholder="Password" />
                                         </Form.Group>
+
+                                        <Form.Group controlId="formBasicPassword">
+                                            <Form.Label>Confirm Password</Form.Label>
+                                            <Form.Control type="password" placeholder="Confirm Password" />
+                                        </Form.Group>
+
+
+
                                         <Button variant="primary" type="submit">
                                             Submit
                                         </Button>
@@ -47,4 +60,4 @@ class Login extends Component {
     }
 }
 
-export default Login;
+export default Signup;
