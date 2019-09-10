25c6ba3a104cf01e31a538e5a98198c5381535b8
Mo-Jo <mo.juseph@gmail.com>
2019-09-09 20:16:52 -0400

    added the login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter , Route } from 'react-router-dom';
 import Main from './components/Main/Main';
+import Login from './components/Login/Login';
+
 
 
 
 class App extends Component {
+  
+
+
+
+
   render(){
     return (
       <>
-      <Main />
+      <BrowserRouter>
+        <div className='App'>
+          <Route exact path='/' component={Main}/>
+          <Route exact path='/login' component={Login}/>
+        </div>
+      </BrowserRouter>
       </>
     )
   }
