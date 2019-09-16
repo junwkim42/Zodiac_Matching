9f1c09513eee537abfecd260396c50f1f2b8ebb6
Daryl Acut <darylacut@yahoo.com>
2019-09-16 15:29:11 -0400

    completed 20 users in seedDB

--- client/src/seedDB.js
+++ client/src/seedDB.js
@@ -12,181 +12,181 @@ const matchesSeed = [
     id: 1,
     name: "Chloie",
     gender: "Female",
-    birthdate: "",
-    age: "",
+    birthdate: "21-Mar-1997",
+    age: 22,
     profilePic: "https://data.whicdn.com/images/302058504/large.jpg?t=1511579073",
-    zodiacSign: ""
+    zodiacSign: "Aries"
   },
   {
     id: 2,
     name: "Hally",
     gender: "Female",
-    birthdate: "",
-    age: "",
+    birthdate: "20-Apr-1996",
+    age: 23,
     profilePic: "https://www.best4geeks.com/wp-content/uploads/2018/08/30-broken-heart-girl-profile-picture-Copy.jpg",
-    zodiacSign: ""
+    zodiacSign: "Taurus"
 },
 {
     id: 3,
     name: "Panda",
     gender: "Female",
-    birthdate: "",
-    age: "",
+    birthdate: "21-May-1998",
+    age: 21,
     profilePic: "https://www.best4geeks.com/wp-content/uploads/2018/07/30-cute-girl-facebook-profile-picture-1.jpg",
-    zodiacSign: ""
+    zodiacSign: "Gemini"
 },
 {
     id: 4,
     name: "Mike",
     gender: "Male",
-    birthdate: "",
-    age: "",
+    birthdate: "21-Jun-1990",
+    age: 29,
     profilePic: "https://www.irreverentgent.com/wp-content/uploads/2018/03/Awesome-Profile-Pictures-for-Guys-look-away2.jpg",
-    zodiacSign: ""
+    zodiacSign: "Cancer"
 },
 {
     id: 5,
     name: "Facebook Man",
     gender: "Male",
-    birthdate: "",
-    age: "",
+    birthdate: "23-Jul-1980",
+    age: 39,
     profilePic: "https://www.sibbaldtraining.com/wp-content/uploads/2017/05/dummy-profile-pic-male1.jpg",
-    zodiacSign: ""
+    zodiacSign: "Leo"
 },
 {
     id: 6,
     name: "Warren",
     gender: "Male",
-    birthdate: "",
-    age: "",
+    birthdate: "23-Aug-2000",
+    age: 19,
     profilePic: "https://pbs.twimg.com/profile_images/740015971025354752/B58eohC3_400x400.jpg",
-    zodiacSign: ""
+    zodiacSign: "Virgo"
 },
 {
     id: 7,
     name: "Angelina",
     gender: "Female",
-    birthdate: "",
-    age: "",
+    birthdate: "23-Sep-1995",
+    age: 24,
     profilePic: "https://i.pinimg.com/originals/be/ac/96/beac96b8e13d2198fd4bb1d5ef56cdcf.jpg",
-    zodiacSign: ""
+    zodiacSign: "Libra"
   },
   {
     id: 8,
     name: "Art",
     gender: "Male",
-    birthdate: "",
-    age: "",
+    birthdate: "26-Oct-1986",
+    age: 33,
     profilePic: "https://twistedsifter.files.wordpress.com/2012/09/trippy-profile-pic-portrait-head-on-and-from-side-angle.jpg?w=800",
-    zodiacSign: ""
+    zodiacSign: "Scorpio"
 },
 {
     id: 9,
     name: "Kate",
     gender: "Female",
-    birthdate: "",
-    age: "",
+    birthdate: "22-Nov-2001",
+    age: 17,
     profilePic: "https://live.staticflickr.com/4606/39945007434_3ec00a2fd4_b.jpg",
-    zodiacSign: ""
+    zodiacSign: "Sagittarius"
 },
 {
     id: 10,
     name: "Maxine",
     gender: "Female",
-    birthdate: "",
-    age: "",
+    birthdate: "21-Sep-1995",
+    age: 24,
     profilePic: "https://steamuserimages-a.akamaihd.net/ugc/263843003897408174/78E02EA5E7CC13D64E16F6C6691B6CC7379123EB/",
-    zodiacSign: ""
+    zodiacSign: "Virgo"
 },
 {
     id: 11,
     name: "Jefferson",
     gender: "Male",
-    birthdate: "",
-    age: "",
+    birthdate: "20-Jan-1982",
+    age: 37,
     profilePic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBxGHaDpIhz6EuI6iYYulYTPFQcWFTvP88xK07Gf-nO5EtSOTl",
-    zodiacSign: ""
+    zodiacSign: "Aquarius"
 },
 {
     id: 12,
     name: "Nathan",
     gender: "Male",
-    birthdate: "",
-    age: "",
+    birthdate: "19-Feb-1999",
+    age: 20,
     profilePic: "https://vignette.wikia.nocookie.net/villains/images/1/1f/Nathan.PNG/revision/latest?cb=20150201064958",
-    zodiacSign: ""
+    zodiacSign: "Pisces"
 },
 {
     id: 13,
     name: "Dana",
     gender: "Female",
-    birthdate: "",
-    age: "",
+    birthdate: "11-Jul-1995",
+    age: 24,
     profilePic: "https://vignette.wikia.nocookie.net/life-is-strange/images/4/49/Dana_Ward_bts.png/revision/latest?cb=20170901184343",
-    zodiacSign: ""
+    zodiacSign: "Cancer"
 },
 {
     id: 14,
     name: "Victoria",
     gender: "Female",
-    birthdate: "",
-    age: "",
+    birthdate: "14-Aug-1995",
+    age: 24,
     profilePic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRYJRzrfeXdwDgOFwHNA2nqAH4H4Jl4bylgSrWAWVvDkwgCBrsOg",
-    zodiacSign: ""
+    zodiacSign: "Leo"
 },
 {
     id: 15,
     name: "Taylor",
     gender: "Female",
-    birthdate: "",
-    age: "",
+    birthdate: "22-Dec-1993",
+    age: 26,
     profilePic: "https://pm1.narvii.com/6542/4543efcebb003dd626140121b940da2e6d803a23_00.jpg",
-    zodiacSign: ""
+    zodiacSign: "Capricorn"
   },
   {
     id: 16,
     name: "Rachel",
     gender: "Female",
-    birthdate: "",
-    age: "",
+    birthdate: "22-May-1994",
+    age: 25,
     profilePic: "https://external-preview.redd.it/tzBgFbGl7aUMSihyE0hbNp5m8_ZXl9jvcN52axLkSxo.jpg?auto=webp&s=9dcf94c439e765c5b7c52360843d5b9c62f3e53b",
-    zodiacSign: ""
+    zodiacSign: "Gemini"
 },
 {
     id: 17,
     name: "Frank",
     gender: "Male",
-    birthdate: "",
-    age: "",
+    birthdate: "31-Aug-1981",
+    age: 38,
     profilePic: "https://vignette.wikia.nocookie.net/life-is-strange/images/d/d7/Frank_Bowers.jpg/revision/latest?cb=20150821114017&path-prefix=de",
-    zodiacSign: ""
+    zodiacSign: "Virgo"
 },
 {
     id: 18,
     name: "Ethan",
     gender: "Male",
-    birthdate: "",
-    age: "",
+    birthdate: "05-Oct-1973",
+    age: 46,
     profilePic: "https://vignette.wikia.nocookie.net/playstationallstarsfanfictionroyale/images/2/21/Ethanmars.jpg/revision/latest?cb=20130409223931",
-    zodiacSign: ""
+    zodiacSign: "Libra"
 },
 {
     id: 19,
     name: "Jayden",
     gender: "Male",
-    birthdate: "",
-    age: "",
+    birthdate: "14-Nov-1978",
+    age: 41,
     profilePic: "https://www.nautiljon.com/images/jeuxvideo_persos/00/15/norman_jayden_2251.jpg?0",
-    zodiacSign: ""
+    zodiacSign: "Scorpio"
 },
 {
     id: 20,
-    name: "Daniel",
+    name: "Sean",
     gender: "Male",
-    birthdate: "",
-    age: "",
+    birthdate: "15-Mar-2000",
+    age: 19,
     profilePic: "https://videochums.com/article/life-is-strange-2-characters.jpg",
-    zodiacSign: ""
+    zodiacSign: "Pisces"
 }
 ];
 
