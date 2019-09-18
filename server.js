7fe231c6bd1f8fbbf92d123018eb89fc1a48adf7
Scott Kim <scottkim1003@gmail.com>
2019-09-18 04:05:39 -0400

    test

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Serve up static assets (usually on heroku)
 // if (process.env.NODE_ENV === "production") {}
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("client/build"));
 
 
 
